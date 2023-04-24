28ad8a49fed928e1d147af6b9333fa800fdf588c
Bhanu-Ganga-DevOPs <Kittu@225>
2023-04-24 00:41:24 -0400

    added catalogue

--- user.sh
+++ user.sh
@@ -1,16 +1,9 @@
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+
 component=user
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+schema_setup=mongo
+func_nodejs
 
-yum install nodejs -y
-
-useradd $app_user
-
-mkdir /app
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-cd /app
-unzip /tmp/${component}.zip
-
-cd /app
-npm install
