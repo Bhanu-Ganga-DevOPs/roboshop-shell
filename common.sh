ee287e7ca9b7258f729d567a9e699c886e228127
Bhanu-Ganga-DevOPs <gangabhavanikatraprthi@gmail.com>
2023-05-31 17:00:05 -0400

    testing

--- common.sh
+++ common.sh
@@ -79,8 +79,9 @@ func_schema_setup() {
     func_stat_check $?
 
     func_print_head "Load Schema"
-     echo " mysql -h mysql-dev.gangabhavanikatraparthi.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file"
+    echo " mysql -h mysql-dev.gangabhavanikatraparthi.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file"
     mysql -h mysql-dev.gangabhavanikatraparthi.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file
+    sleep 30
     func_stat_check $?
   fi
 }
